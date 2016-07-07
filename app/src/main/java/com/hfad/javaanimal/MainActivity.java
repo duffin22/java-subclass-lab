e747a8a9fd5233b3caf30aa684d6d5b5abb0e519
Matthew Duffin <matthewtduffin@gmail.com>
2016-07-07 09:09:02 -0700

    Spellings fixed

--- app/src/main/java/com/hfad/javaanimal/MainActivity.java
+++ app/src/main/java/com/hfad/javaanimal/MainActivity.java
@@ -1,8 +1,13 @@
 package com.hfad.javaanimal;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_main);
 
         vultureButton=(Button) findViewById(R.id.vultureBut);
@@ -111,6 +121,9 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(View view) {
                                 Toast.makeText(getBaseContext(),R.string.foxNoise, Toast.LENGTH_LONG).show();
+                                Intent intent = getIntent();
+                                finish();
+                                startActivity(intent);
                             }
                         });
                         isColorful=false;
